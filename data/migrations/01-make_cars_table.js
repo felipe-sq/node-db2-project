15cc9a21378ae9b70365314d4d47f943d8904e47
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-20 14:58:41 -0400

    project set up on name branch, npm dependencies installed. Wrote cars schema for up and down functions inside migration file.

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,19 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id').primary();
+    tbl.text('vin', 128).notNullable().unique().required();
+    tbl.text('make').required();
+    tbl.text('model').required();
+    tbl.numeric('mileage').required();
+    tbl.text('title');
+    tbl.text('transmission');
+
+    tbl.integer('year');
+    tbl.integer('owner_id').references('id').inTable('owners');
+    tbl.timestamps();
+  }
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
